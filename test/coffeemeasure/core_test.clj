a449124482a8e658fc51bd28eea600ed3af5a97e
jmignault <john.mignault@gmail.com>
2018-01-19 09:30:33 -0500

    Initial tests

--- test/coffeemeasure/core_test.clj
+++ test/coffeemeasure/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [coffeemeasure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest oz2g-test
+  (testing "Test ounces to grams"
+    (is (= (oz2g 16) 453.592))))
+
+(deftest g2oz-test
+  (testing "Test grams to ounces"
+    (is (= (g2oz 454) 16.014396))))
+
+(deftest coffemeasure-test
+  (testing "Test coffeemeasure"
+    (is (= (coffeemeasure 16) 27.0))))
