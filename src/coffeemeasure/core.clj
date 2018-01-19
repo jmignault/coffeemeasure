b374fb5958a67e7a09b9fdb8f20ecbff27113ea8
jmignault <john.mignault@gmail.com>
2018-01-19 09:30:18 -0500

    Initial code

--- src/coffeemeasure/core.clj
+++ src/coffeemeasure/core.clj
@@ -1,6 +1,19 @@
 (ns coffeemeasure.core)
 
-(defn foo
-  "I don't do a whole lot."
+;; Coffee to water ratio
+(def RATIO 17.1)
+
+(defn oz2g
+  "Convert x ounces to grams"
   [x]
-  (println x "Hello, World!"))
+  (* x 28.3495))
+
+(defn g2oz
+  "Convert x grams to ounces"
+  [x]
+  (* x 0.035274))
+
+(defn coffeemeasure
+  "How many grams of coffee makes x ounces"
+  [x]
+  (java.lang.Math/ceil (/  (oz2g x) RATIO)))
