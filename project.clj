da552e77691f681f5e9369444a3e6dabeb5832f9
jmignault <john.mignault@gmail.com>
2018-01-19 09:29:20 -0500

    Edit project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject coffeemeasure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Determine the right weight of coffee for the given amount of water"
+  :url "http://johnmignault.com/coffeemeasure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
