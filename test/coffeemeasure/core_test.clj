eeb23bbaa454fbda2e3ee15eef6c1c7628daf5ec
jmignault <john.mignault@gmail.com>
2021-10-23 12:24:16 -0400

    Add test for coffeeprint, fix coffeemeasure test fn name

--- test/coffeemeasure/core_test.clj
+++ test/coffeemeasure/core_test.clj
@@ -6,12 +6,16 @@
 
 (deftest oz2g-test
   (testing "Test ounces to grams"
-    (is (= (oz2g 16) 453.592))))
+    (is (= (oz2g 16) 454))))
 
 (deftest g2oz-test
   (testing "Test grams to ounces"
     (is (= (g2oz 454) 16.014396))))
 
-(deftest coffemeasure-test
+(deftest coffeemeasure-test
   (testing "Test coffeemeasure"
-    (is (= (coffeemeasure 16) 27))))
+    (is (= (coffeemeasure 454) 29))))
+
+(deftest coffeeprint-test
+  (testing "Test coffeeprint"
+    (is (= (coffeeprint 16) "454g (16 oz) of made coffee requires 29g of ground coffee."))))
